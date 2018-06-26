6ba49d5d52f2111f5abaaafbd4f2e7473daeb899
GaHyeon <rkgus6545@naver.com>
2018-06-26 16:34:48 +0900

    instagram: like(heart) + like_num change

--- main/java/com/example/rkgus/insta_/MainActivity.java
+++ main/java/com/example/rkgus/insta_/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +15,19 @@ public class MainActivity extends AppCompatActivity {
     ImageButton btn_like2;
     ImageButton btn_like3;
     int i=1, j=1, k=1;
+    TextView like_num1;
+    TextView like_num2;
+    TextView like_num3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        like_num1 = (TextView)findViewById(R.id.like);
+        like_num2 = (TextView)findViewById(R.id.like2);
+        like_num3 = (TextView)findViewById(R.id.like3);
 
         btn_like1 = (ImageButton) findViewById(R.id.heart_img1);
         btn_like2 = (ImageButton) findViewById(R.id.heart_img2);
@@ -35,22 +45,26 @@ public class MainActivity extends AppCompatActivity {
             if(view.getId()==R.id.heart_img1) {
                 if (i==1) {
                     btn_like1.setImageResource(R.drawable.ic_btn_heart_clicked);
+                    like_num1.setText(""+i);
                     i--;
                 }
                 else {
                     btn_like1.setImageResource(R.drawable.ic_btn_heart);
+                    like_num1.setText(""+i);
                     i++;
                 }
             }
-            //여러 게시글 좋아요가 왜 안될까.
+
             //feed2 like
             if(view.getId()==R.id.heart_img2) {
                 if (j==1) {
                     btn_like2.setImageResource(R.drawable.ic_btn_heart_clicked);
+                    like_num2.setText(""+j);
                     j--;
                 }
                 else {
                     btn_like2.setImageResource(R.drawable.ic_btn_heart);
+                    like_num2.setText(""+j);
                     j++;
                 }
             }
@@ -59,10 +73,12 @@ public class MainActivity extends AppCompatActivity {
             if(view.getId()==R.id.heart_img3) {
                 if (k==1) {
                     btn_like3.setImageResource(R.drawable.ic_btn_heart_clicked);
+                    like_num3.setText(""+k);
                     k--;
                 }
                 else {
                     btn_like3.setImageResource(R.drawable.ic_btn_heart);
+                    like_num3.setText(""+k);
                     k++;
                 }
             }
